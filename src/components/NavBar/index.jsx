7921a31dd7a8a1ac436d0d83c940092cb106e7a0
francopachec0 <francopacheco.dev@gmail.com>
2022-09-13 11:45:16 -0300

    changes

---
+++ src/components/NavBar/index.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import {
+  Nav,
+  NavBarContainer,
+  NavLogo,
+  MobileIcon,
+  NavMenu,
+  NavItem,
+  NavLinks,
+  NavBtn,
+  NavBtnLink,
+} from "./NavBarElements";
+import { FaBars } from "react-icons/fa";
+
+const NavBar = () => {
+  return (
+    <>
+      <Nav>
+        <NavBarContainer>
+          <NavLogo to="/">PACHECO Impresiones</NavLogo>
+          <MobileIcon>
+            <FaBars />
+          </MobileIcon>
+          <NavMenu>
+            <NavItem>
+              <NavLinks to="about">Sobre Nosotros</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="services">Servicios</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="ubication">Ubicación</NavLinks>
+            </NavItem>
+          </NavMenu>
+          <NavBtn>
+            <NavBtnLink to='contact'>
+              ¡Contactanos!
+            </NavBtnLink>
+          </NavBtn>
+        </NavBarContainer>
+      </Nav>
+    </>
+  );
+};
+
+export default NavBar;
