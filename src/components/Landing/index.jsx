272cb251aa6148ad6cf69b110f73d5caef00789c
francopachec0 <francopacheco.dev@gmail.com>
2022-09-19 12:54:45 -0300

    changes

--- src/components/Landing/index.jsx
+++ src/components/Landing/index.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from "react";
+import {
+  LandingContainer,
+  LandingBg,
+  VideoBg,
+  LandingContent,
+  LandingH1,
+  LandingP,
+  LandingBtnWrapper,
+  ArrowForward,
+  ArrowRight,
+} from "./LandingElements";
+import Video from "../../video/video3.mp4";
+import { useState } from "react";
+import { Button } from "../ButtonElements";
 
 export const Landing = () => {
+  const [hover, setHover] = useState(false);
+
+  const onHover = () => {
+    setHover(!hover);
+  };
+
   return (
-    <>
-        Landing
-    </>
-  )
-}
+    <LandingContainer>
+      <LandingBg>
+        <VideoBg autoPlay loop muted src={Video} type="video/mp4" />
+      </LandingBg>
+      <LandingContent>
+        <LandingH1>Aqui va un H1</LandingH1>
+        <LandingP>Este seria un parrafo P que lo tengo q pensar todavia</LandingP>
+        <LandingBtnWrapper>
+          <Button
+            to="contact"
+            onMouseEnter={onHover}
+            onMouseLeave={onHover}
+            primary="true"
+            dark="true"
+          >
+            ¡CONTACTANOS! {hover ? <ArrowForward /> : <ArrowRight />}
+          </Button>
+        </LandingBtnWrapper>
+      </LandingContent>
+    </LandingContainer>
+  );
+};
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Landing } from "../components/Landing";
 import NavBar from "../components/NavBar";
 import SideBar from "../components/SideBar";
 
@@ -13,6 +14,7 @@ const Home = () => {
     <>
       <SideBar isOpen={isOpen} toggle={toggle} />
       <NavBar toggle={toggle} />
+      <Landing />
     </>
   );
 };
